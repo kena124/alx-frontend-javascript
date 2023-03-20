f5b1e0f9798e36087e4ba46468eead53fd7b7779
root <kena1224>
2023-03-20 14:21:59 -0700

    fixed

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,3 +1,15 @@
+export function taskFirst() {
+  const task = 'I prefer const when I can.';
+  return task;
+}
 
-TaskFirst: ${taskFirst()}`);
-console.log(`TaskNext: ${taskNext()}`);
+export function getLast() {
+  return ' is okay';
+}
+
+export function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
+}
